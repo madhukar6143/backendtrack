7331625f37b1d13a0afab07dbecea7deb4357a3e
madhukar eppalapelly <madhukar.eppalapelly@gmail.com>
2023-01-18 01:46:28 +0530

    first commit

--- server.js
+++ server.js
@@ -1,28 +1,55 @@
 const express = require("express");
 const app = express();
 const requestIp = require('request-ip');
-const axios =require("axios")
+const axios = require("axios")
 const ipfetch = require('ip-fetch');
+const mongoClient = require("mongodb").MongoClient;
+const dbConnectionString = "mongodb+srv://madhu:madhu@clusterbackend.szevd.mongodb.net/myfirstdb?retryWrites=true&w=majority"
+let dataCollectionObject;
 
 
-
- 
 const cors = require('cors')
-const corsOptions ={
-    origin:["http://localhost:3000","https://trackfrontend.netlify.app"], 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
+const corsOptions = {
+    origin: ["http://localhost:3000", "https://trackfrontend.netlify.app"],
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200
 }
 app.use(cors(corsOptions));
 
 
+mongoClient.connect(dbConnectionString)
+    .then(client => {
+        //create DB object
+        const dbObj = client.db("portfolio");
+        //get collection object
+
+        dataCollectionObject = dbObj.collection("locationData")
+        //share userCollectionObj
+        console.log("Connected to locationDB ")
+    })
+    .catch(err => console.log("err in connecting to DB ", err))
+
+
 app.use(requestIp.mw());
-app.use(async(req, res) =>{
+app.use(async (req, res) => {
     const clientIp = req.clientIp;
-      let info = await ipfetch.getLocationNpm(clientIp);
-        res.json({ip:info});
+    let info = await ipfetch.getLocationNpm("124.123.186.218");
+    reqObj =
+    {
+        "country": info.country,
+        "region": info.region,
+        "regionName": info.regionName,
+        "city": info.city,
+        "zip": info.zip,
+        "lat": info.lat,
+        "lon": info.lon,
+        "org": info.org,
+        "as": info.as,
+        "query": info.query
+    }
+    await dataCollectionObject.insertOne(reqObj);
 });
 
 
-const port=3000;
-app.listen(port,()=>console.log("server on port 3000..."))
+const port = 3000;
+app.listen(port, () => console.log("server on port 3000..."))
