d7c16f8b82212baa698fc44ae947e0607bffaa24
madhukar eppalapelly <madhukar.eppalapelly@gmail.com>
2023-01-17 15:48:13 +0530

    first commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const requestIp = require('request-ip');
 const app = express();
 const cors = require('cors')
 const corsOptions ={
-    origin:["http://localhost:3001"], 
+    origin:["http://localhost:3001","https://trackfrontend.netlify.app"], 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
