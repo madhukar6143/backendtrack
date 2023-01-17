f13fd93cb5d42c6ccd1316396887e4c03c835062
madhukar eppalapelly <madhukar.eppalapelly@gmail.com>
2023-01-17 15:44:09 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const requestIp = require('request-ip');
+const app = express();
+const cors = require('cors')
+const corsOptions ={
+    origin:["http://localhost:3001"], 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
+app.use(requestIp.mw());
+app.use(function(req, res) {
+    const clientIp = req.clientIp;
+    console.log(clientIp)
+    res.json({ip:clientIp});
+});
+const port=3000;
+app.listen(port,()=>console.log("server on port 3000..."))
