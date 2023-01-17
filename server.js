8db4d75131a41b160277f3ed6b5c0809796373d5
madhukar eppalapelly <madhukar.eppalapelly@gmail.com>
2023-01-18 02:19:03 +0530

    first commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ let dataCollectionObject;
 
 const cors = require('cors')
 const corsOptions = {
-    origin: ["http://localhost:3000", "https://trackfrontend.netlify.app"],
+    origin: ["https://madhukar-eppalapelly.netlify.app", "https://trackfrontend.netlify.app"],
     credentials: true,            //access-control-allow-credentials:true
     optionSuccessStatus: 200
 }
