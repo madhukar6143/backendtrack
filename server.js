57c38991e5ad0c9f5d5079590595fbe7f9790a33
madhukar eppalapelly <madhukar.eppalapelly@gmail.com>
2023-01-19 00:04:36 +0530

    first commit

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const dbConnectionString = "mongodb+srv://madhu:madhu@clusterbackend.szevd.mongo
 let dataCollectionObject;
 
 
+
 const cors = require('cors')
 const corsOptions = {
     origin: ["https://madhukar-eppalapelly.netlify.app", "https://trackfrontend.netlify.app"],
@@ -17,40 +18,66 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 
-mongoClient.connect(dbConnectionString)
-    .then(client => {
-        //create DB object
-        const dbObj = client.db("portfolio");
-        //get collection object
-
-        dataCollectionObject = dbObj.collection("locationData")
-        //share userCollectionObj
-        console.log("Connected to locationDB ")
-    })
-    .catch(err => console.log("err in connecting to DB ", err))
-
-
 app.use(requestIp.mw());
 app.use(async (req, res) => {
+    await mongoClient.connect(dbConnectionString)
+        .then(client => {
+            //create DB object
+            const dbObj = client.db("portfolio");
+            //get collection object
+
+            dataCollectionObject = dbObj.collection("locationData")
+            //share userCollectionObj
+            console.log("Connected to locationDB ")
+        })
+        .catch(err => console.log("err in connecting to DB ", err))
+
+
     const clientIp = req.clientIp;
     let info = await ipfetch.getLocationNpm(clientIp);
-    reqObj =
-    {
-        "country": info.country,
-        "region": info.region,
-        "regionName": info.regionName,
-        "city": info.city,
-        "zip": info.zip,
-        "lat": info.lat,
-        "lon": info.lon,
-        "org": info.org,
-        "as": info.as,
+    let userOfDB = await dataCollectionObject.findOne({
         "query": info.query
+    });
+    if (userOfDB !== null) {
+        userOfDB = { ...userOfDB, count: userOfDB.count + 1 }
+        let res = await dataCollectionObject.updateOne(
+            { query: userOfDB.query },
+            { $set: { ...userOfDB } }
+        );
+        //if date existed update
+
+        //response.send({ message: "Message Sentvhgvghhgfhj" });
     }
-    await dataCollectionObject.insertOne(reqObj);
-    res.send({ip:clientIp})
-});
+    //if user not existed add new
+    else {
+        reqObj =
+        {
+            "country": info.country,
+            "region": info.region,
+            "regionName": info.regionName,
+            "city": info.city,
+            "zip": info.zip,
+            "lat": info.lat,
+            "lon": info.lon,
+            "org": info.org,
+            "as": info.as,
+            "query": info.query,
+            "count": 1
+        }
+        await dataCollectionObject.insertOne(reqObj);
+        // response.send({ message: "Message Sent" });
+        //send res
+    }
+    res.send({message:"visit https://madhukar-eppalapelly.netlify.app"})
+}
+)
 
 
 const port = 3000;
 app.listen(port, () => console.log("server on port 3000..."))
+
+
+
+
+
+
